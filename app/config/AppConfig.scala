1e52e0d4be13a569cdc46b97694bb4c59b3ad78c
Alexander Ryblov <ryblov.a.v@gmail.com>
2016-06-08 08:12:03 +0300

    move script.sql to /conf folder

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -1,11 +1,16 @@
 package config
 
 import java.text.SimpleDateFormat
+import play.Logger._
 
 object AppConfig {
+
+  val root = System.getProperty("user.dir")
+
   val countPartition = 8
   val partition = 1000
 
+  val scriptPath   = s"$root//conf//queries//script.sql"
   val templatePath = s"$root//conf//templates//template.xlsx"
 
   val COMPANY_TAG = "<COMPANY>"
